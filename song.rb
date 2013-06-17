04adf4eb2ba08ba345d6122d84f7c2d7dd1ac0d8
Mentessi <mentessi@gmail.com>
2013-06-17 11:33:21 +0100

    move configure to main.rb

--- song.rb
+++ song.rb
@@ -1,13 +1,5 @@
 require 'data_mapper'
 
-configure :development do
-	DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
-end
-
-configure :production do 
-	DataMapper.setup(:default, ENV['DATABASE_URL'])
-end
-
 class Song
 	include DataMapper::Resource
 	property :id, Serial
